13729d44bba314d5f8a6a9491fb631c45cfdfbfa
fjfh06 <fferher513h@g.educaand.es>
2025-04-06 18:06:58 +0200

    Terminado TeclaFactory

--- src/jd/piano/teclas/TeclaFactory.java
+++ src/jd/piano/teclas/TeclaFactory.java
@@ -3,6 +3,16 @@ package jd.piano.teclas;
 public class TeclaFactory {
     private TeclaFactory(){}
     public static Tecla crearTecla(int nota){
-        throw new UnsupportedOperationException("Sin programar");
+        Tecla returnarero = null;
+        if (nota<=128){
+            returnarero = switch (nota%12){
+                case 1, 3, 6, 8, 10-> new TeclaNegra(nota);
+                case 0, 5 -> new TeclaBlanca1(nota);
+                case 2, 7, 9 -> new TeclaBlanca2(nota);
+                case 4, 11 -> new TeclaBlanca3(nota);
+                default ->null;
+            };
+        }
+        return returnarero;
     }
 }
