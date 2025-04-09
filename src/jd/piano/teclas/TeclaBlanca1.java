89a159e2a81701e73aab3a81cb889d4ac2e44cec
fjfh06 <fferher513h@g.educaand.es>
2025-04-09 09:59:45 +0200

    Arreglada la forma de TeclaBlanca1

--- src/jd/piano/teclas/TeclaBlanca1.java
+++ src/jd/piano/teclas/TeclaBlanca1.java
@@ -9,13 +9,13 @@ public class TeclaBlanca1 extends TeclaBlanca{
     protected int[] getVerticesX() {
         int x = this.posicion.x;
         int anchura = this.getAnchura();
-        return new int[]{x, x, x + anchura, x + anchura};
+        return new int[]{x, x, x + anchura,x+anchura,(x+anchura)-(TeclaNegra.ANCHURA/2),(x+anchura)-(TeclaNegra.ANCHURA/2)};
     }
 
     @Override
     protected int[] getVerticesY() {
         int y = this.posicion.y;
         int altura = this.getAltura();
-        return new int[]{y, y + altura, y + altura, y};
+        return new int[]{y, y + altura, y + altura,y+TeclaNegra.ALTURA,y+TeclaNegra.ALTURA,y};
     }
 }
